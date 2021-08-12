27800cbbff71874365d4ac9e304aa88000b4d669
jwrunner <jwrunner7@gmail.com>
2021-08-11 17:56:48 -0700

    update github to build package

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Runner-UI
 Components I use across projects
+`npm i -D @jwrunner/svelte-ui`
 
 ## To update NPM package
 Run `npm version patch` which will bump from 1.0.2 to 1.0.3 for example and then commit the changes, and push changes to GitHub which will automatically fire off an NPM publish
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,8 @@
 import Store from './Store.svelte';
 import Button from './Button.svelte';
-export { Store, Button };
+import Copy from './Button.svelte';
+import ShowHide from './Button.svelte';
+export { Store, Button, Copy, ShowHide };
 
 // For one-offs to export a default component
 // import Store from './Store.svelte';
