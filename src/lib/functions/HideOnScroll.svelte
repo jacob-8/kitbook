d5d3001e01a14abbca14ced157563a648582d9a1
jwrunner <jwrunner7@gmail.com>
2021-08-13 17:47:48 -0700

    hide on scroll v1

---
+++ src/lib/functions/HideOnScroll.svelte
@@ -0,0 +1,60 @@
+<script>
+  export let duration = '300ms';
+  export let offset = 0;
+  export let tolerance = 0;
+
+  let headerClass = 'show';
+  let y = 0;
+  let lastY = 0;
+
+  function deriveClass(y, dy) {
+    if (y < offset) {
+      return 'show';
+    }
+
+    if (Math.abs(dy) <= tolerance) {
+      return headerClass;
+    }
+
+    if (dy < 0) {
+      return 'show';
+    }
+
+    return 'hide';
+  }
+
+  function updateClass(y) {
+    const dy = lastY - y;
+    lastY = y;
+    return deriveClass(y, dy);
+  }
+
+  function setTransitionDuration(node: HTMLElement) {
+    node.style.transitionDuration = duration;
+  }
+
+  $: headerClass = updateClass(y);
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<div use:setTransitionDuration class={headerClass}>
+  <slot />
+</div>
+
+<style>
+  div {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    transition: transform 300ms linear;
+  }
+  .show {
+    transform: translateY(0%);
+  }
+  .hide {
+    transform: translateY(-100%);
+  }
+</style>
+
+<!-- From https://presstige.io/p/Hide-header-on-scroll-in-Svelte-219cc979e826459a97ede99c7c7fb09b -->
