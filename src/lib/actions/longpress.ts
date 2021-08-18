2908fa13819e18b528037c4bd67a3f02467e0092
jwrunner <jwrunner7@gmail.com>
2021-08-17 20:00:00 -0700

    improve longpress to include shortpress so a button can serve dual-purpose

--- src/lib/actions/longpress.ts
+++ src/lib/actions/longpress.ts
@@ -1,32 +1,39 @@
 export function longpress(node: Node, duration = 500) {
-    let timer;
+  let start: number; // for shortpress
+  let shortpressed: number; // so touchend and mouseup events don't both emit on touch devices
+  let timer; // for longpress
 
-    const handleDown = () => {
-        timer = setTimeout(() => {
-            node.dispatchEvent(
-                new CustomEvent('longpress')
-            );
-        }, duration);
-    };
+  const handleDown = () => {
+    start = Date.now();
 
-    const handleUp = () => {
-        clearTimeout(timer)
-    };
+    timer = setTimeout(() => {
+      node.dispatchEvent(new CustomEvent('longpress'));
+    }, duration);
+  };
 
-    node.addEventListener('mousedown', handleDown);
-    node.addEventListener('mouseup', handleUp);
-    node.addEventListener('touchstart', handleDown);
-    node.addEventListener('touchend', handleUp);
+  const handleUp = () => {
+    if (!(shortpressed > Date.now() - duration / 2) && Date.now() - start < duration) {
+      node.dispatchEvent(new CustomEvent('shortpress'));
+      shortpressed = Date.now(); 
+    }
 
-    return {
-        update(newDuration) {
-            duration = newDuration;
-        },
-        destroy() {
-            node.removeEventListener('mousedown', handleDown);
-            node.removeEventListener('mouseup', handleUp);
-            node.addEventListener('touchstart', handleDown);
-            node.addEventListener('touchend', handleUp);
-        }
-    };
+    clearTimeout(timer);
+  };
+
+  node.addEventListener('mousedown', handleDown);
+  node.addEventListener('mouseup', handleUp);
+  node.addEventListener('touchstart', handleDown);
+  node.addEventListener('touchend', handleUp);
+
+  return {
+    update(newDuration: number) {
+      duration = newDuration;
+    },
+    destroy() {
+      node.removeEventListener('mousedown', handleDown);
+      node.removeEventListener('mouseup', handleUp);
+      node.addEventListener('touchstart', handleDown);
+      node.addEventListener('touchend', handleUp);
+    },
+  };
 }
