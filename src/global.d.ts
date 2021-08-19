0c060bf79598555027e7522d15369b1e6e71a130
jwrunner <jwrunner7@gmail.com>
2021-08-19 15:13:39 -0700

    add clickOutside

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,6 +4,7 @@ declare namespace svelte.JSX {
   interface HTMLAttributes<T> {
     onlongpress?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
     onshortpress?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
+    onclickedOutside?: (event: CustomEvent<any> & { target: EventTarget & T }) => any;
   }
 }
 
