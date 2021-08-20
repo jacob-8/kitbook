14ac3d47052c3fbb776cc6cc207dc2295b56b50d
jwrunner <jwrunner7@gmail.com>
2021-08-20 14:39:31 -0700

    add Menu component

---
+++ src/lib/shell/Menu.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { fly } from 'svelte/transition';
+  // import { portal } from '$lib/actions/portal';
+</script>
+
+<!-- use:portal if needed to solve z-index issues, putting menu on the document body doesn't work great in Svench-->
+<div
+  transition:fly={{ y: -10, duration: 150 }}
+  class="{$$props.class} absolute z-30 mt-2 w-48 rounded-md
+shadow-lg">
+  <div class="sv-menu">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .sv-menu {
+    @apply py-1 rounded-md bg-white flex flex-col;
+  }
+  :global(.sv-menu a),
+  :global(.sv-menu button) {
+    @apply text-left px-4 py-2 text-sm text-gray-700 hover:bg-gray-100
+      transition ease-in-out duration-150;
+  }
+</style>
