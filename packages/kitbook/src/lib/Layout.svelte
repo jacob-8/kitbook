17e7d28e5e1df6c5655b00607efcb9097b03a569
jacobbowdoin <jwrunner7@gmail.com>
2022-10-12 12:20:31 +0800

    add reset back into Kitbook app.html

--- packages/kitbook/src/lib/Layout.svelte
+++ packages/kitbook/src/lib/Layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import './styles/prism-vsc-dark-plus.css';
-  import './tw-prose.css';
+  import './styles/tw-prose.css';
 
   import { page } from '$app/stores';
   import Header from './Header.svelte';
@@ -51,6 +51,17 @@
 
 <style>
   :global(pre) {
-    --at-apply: !-mx-3 md:!mx-0 !rounded-none md:!rounded-md
+    /* ! not working in transformer directives yet */
+    /* --at-apply: !-mx-3 !md:mx-0 !rounded-none !md:rounded-md */
+    margin-left: -0.75rem !important;
+    margin-right: -0.75rem !important;
+    border-radius: 0rem !important;
+  }
+  @media (min-width: 768px) {
+    :global(pre) {
+      margin-left: 0rem !important;
+      margin-right: 0rem !important;
+      border-radius: 0.375rem !important;
+    }
   }
 </style>
