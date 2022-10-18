2026da66fedafe85fc4d6ac83f3afcb46405d39d
jacobbowdoin <jwrunner7@gmail.com>
2022-10-18 14:17:22 +0800

    prepareHref handles urls inside strings, inside brackets, and returns 1st url if 2 found

--- packages/svelte-pieces/src/lib/functions/DetectUrl.svelte
+++ packages/svelte-pieces/src/lib/functions/DetectUrl.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
+  import { prepareDisplay, prepareHref } from './detectUrl';
   export let string: string;
-
-  function prepareDisplay(s: string) {
-    const urlRegex = /^(((https?:\/\/)|(www\.))[^\s]+)/g;
-    if (urlRegex.test(s)) {
-      return s.replace(/https?:\/\//, '');
-    } else {
-      return s;
-    }
-  }
-
-  function prepareHref(s: string) {
-    const urlRegex = /^(((https?:\/\/)|(www\.))[^\s]+)/g;
-    if (urlRegex.test(s)) {
-      return s.replace(/^www\./, 'http://');
-    } else {
-      return null;
-    }
-  }
 </script>
 
 <slot display={prepareDisplay(string)} href={prepareHref(string)} />
---
+++ packages/svelte-pieces/src/lib/functions/detectUrl.ts
@@ -0,0 +1,40 @@
+const urlRegex = /(((https?:\/\/)|(www\.))[^\s>]+)/g;
+
+export function prepareDisplay(s: string) {
+  if (urlRegex.test(s)) {
+    return s.replace(/https?:\/\//, '');
+  } else {
+    return s;
+  }
+}
+
+export function prepareHref(s: string) {
+  const match = s?.match(urlRegex);
+  if (match?.length) {
+    return match[0].replace(/^www\./, 'http://');
+  } else {
+    return null;
+  }
+}
+
+if (import.meta.vitest) {
+  test('prepareHref handles https, http, www', () => {
+    expect(prepareHref("https://google.com")).toMatchInlineSnapshot('"https://google.com"');
+    expect(prepareHref("http://google.com")).toMatchInlineSnapshot('"http://google.com"');
+    expect(prepareHref("www.google.com")).toMatchInlineSnapshot('"http://google.com"');
+  });
+
+  test('prepareHref handles urls inside strings, inside brackets, and returns 1st url if 2 found', () => {
+    expect(prepareHref("Source: <https://creativecommons.org/licenses/by-sa/2.5>, via Wikimedia Commons, http://google.com")).toMatchInlineSnapshot('"https://creativecommons.org/licenses/by-sa/2.5"');
+  });
+
+  test('prepareHref handles no match and undefined', () => {
+    expect(prepareHref("Foo")).toMatchInlineSnapshot('null');
+    expect(prepareHref(undefined)).toMatchInlineSnapshot('null');
+  });
+  
+  test('prepareDisplay handles no match', () => {
+    expect(prepareDisplay("Foo")).toMatchInlineSnapshot('"Foo"');
+    expect(prepareDisplay(undefined)).toMatchInlineSnapshot('undefined');
+  });
+}
