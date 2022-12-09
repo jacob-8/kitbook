680f4b385320fae29eedfb7cc411f87b0c06c286
jacob-8 <jwrunner7@gmail.com>
2022-12-09 16:06:29 +0800

    hardcode hide 'routes for copying'

--- packages/kitbook/src/docs/9-maintainer-notes/4-todo.md
+++ packages/kitbook/src/docs/9-maintainer-notes/4-todo.md
@@ -3,6 +3,7 @@
 ## Important before considering this a publicly shareable project
 *(you have been warned, early-adopters)*
 
+- make pages composed of insertable components so they are simple to copy into a new Kitbook
 - Easily adjust screen size use full-screen view
 - Story layout improvements (learn from svench and https://bookit.leveluptutorials.com/book/A%20Parent-Basic and implement own ideas)
 - Bake prism css into Layout w/ optional flag (currently in app.html)
@@ -15,13 +16,16 @@
 - Easy markdown bookmark hyperlinks
   - FoamBubble like linking, backlinks, and connections graph
 - Scrollspy TOC in sidebar (maybe client-side only for setup simplicity)
-- solve over-applied global scrollbar css
 - use svelte-knobby to beef up knobs, show to the right side and change to show knobs for all visible stories as user scrolls
 - pass in component's code to view in a story
 - consider setting up an options object that can be passed to kitbook via stuff to make future additions easy to implement
   - for example: optional stackblitz icon to quickly open a kitbook in stackblitz (will only work for kitbooks that don't depend on packages in other parts of a monorepo) 
 - could refactor to use activePage instead of activeUrl to make code simpler
 - dynamic slots: https://github.com/sveltejs/svelte/issues/2588 && https://github.com/sveltejs/svelte/pull/4296
-- edit on Gitpod link?
 - Dark mode
 - Investigate [Histoire plugin](https://github.com/histoire-dev/histoire/tree/main/packages/histoire-plugin-screenshot) or [Viteshot](https://viteshot.com/) for visual regression screenshot testing
+- edit on Gitpod link?
+
+## At some point
+- add option to easily hide a directory (and refactor out hardcoded hide of 'routes for copying')
+- global scrollbar css will affect stories not using sandbox (is this even an issue?)
--- packages/kitbook/src/lib/sidebar/Folder.svelte
+++ packages/kitbook/src/lib/sidebar/Folder.svelte
@@ -9,32 +9,37 @@
   const isRootFolder = folder.name === '.';
   let actualExpandedState = activeURL.indexOf(folder.url) !== -1 || expanded;
   $: active = activeURL.indexOf(folder.url) !== -1;
+
+  const internalHiddenFolder = folder.name === 'routes for copying';
 </script>
 
-{#if !isRootFolder}
-  <button type="button"
-    class="hover:text-blue-700 capitalize pr-3 font-semibold cursor-pointer flex text-sm"
-    class:text-blue-800={active}
-    style="padding-left: calc(0.75rem * {folder.depth - 1}"
-    on:click={() => (actualExpandedState = !actualExpandedState)}
-  >
-    <span
-      class="border-l border-gray-300 hover:border-blue-700 pr-3"
-      class:border-dotted={!active}
-    />
-    <span class="py-1">
-      {folder.name}
-    </span>
-  </button>
-{/if}
+{#if !internalHiddenFolder}
+  {#if !isRootFolder}
+    <button
+      type="button"
+      class="hover:text-blue-700 capitalize pr-3 font-semibold cursor-pointer flex text-sm"
+      class:text-blue-800={active}
+      style="padding-left: calc(0.75rem * {folder.depth - 1}"
+      on:click={() => (actualExpandedState = !actualExpandedState)}
+    >
+      <span
+        class="border-l border-gray-300 hover:border-blue-700 pr-3"
+        class:border-dotted={!active}
+      />
+      <span class="py-1">
+        {folder.name}
+      </span>
+    </button>
+  {/if}
 
-{#if isRootFolder || actualExpandedState}
-  {#each folder.pages as page}
-    {#if page.url !== '/README' && page.url !== '/docs/index'}
-      <Page {page} {activeURL} depth={folder.depth} />
-    {/if}
-  {/each}
-  {#each folder.folders as subfolder}
-    <svelte:self folder={subfolder} {expanded} {activeURL} />
-  {/each}
+  {#if isRootFolder || actualExpandedState}
+    {#each folder.pages as page}
+      {#if page.url !== '/README' && page.url !== '/docs/index'}
+        <Page {page} {activeURL} depth={folder.depth} />
+      {/if}
+    {/each}
+    {#each folder.folders as subfolder}
+      <svelte:self folder={subfolder} {expanded} {activeURL} />
+    {/each}
+  {/if}
 {/if}
