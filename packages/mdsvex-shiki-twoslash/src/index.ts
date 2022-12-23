fa3c3e4044d416261dddc2d6d15bcbc093ce74fc
jacob-8 <jwrunner7@gmail.com>
2022-12-23 08:37:48 +0800

    properly output multiple themes

--- packages/mdsvex-shiki-twoslash/src/index.ts
+++ packages/mdsvex-shiki-twoslash/src/index.ts
@@ -1,15 +1,16 @@
 import { lex, parse } from "fenceparser";
 import { renderCodeToHTML, runTwoSlash, createShikiHighlighter, type UserConfigSettings } from "shiki-twoslash";
-import type { Highlighter, HighlighterOptions } from 'shiki';
+import { getHighlighter, Highlighter, HighlighterOptions } from 'shiki';
 import { escapeSvelte } from "mdsvex";
 
-const DEFAULT_THEMES = ['dark-plus', 'light-plus']
+const DEFAULT_THEMES = ['dark-plus', 'light-plus'];
 
 /**
  * Default themes found here: https://www.runpkg.com/?shiki@0.12.1/themes/dark-plus.json
- * Does not support using ```twoslash include foo... as described in https://shikijs.github.io/twoslash/ - not sure if this is possible with a MDSvex highlighter as it works on a code block by code block basis and not file by file as a remark plugin would (i.e. remark-shiki-twoslash)
- * @src https://github.com/pngwn/MDsveX/issues/212#issuecomment-937574889
- * @src https://github.com/shikijs/twoslash/blob/fbf061261fcda90c46e946ce1e2e9357d465c145/packages/remark-shiki-twoslash/src/index.ts#L172-L215
+ * Does not support using ```twoslash include foo... as described in https://shikijs.github.io/twoslash/ - not sure if this is possible with MDSvex as the MDSvex highlighter works on a code block by code block basis and not file by file as a remark plugin would (i.e. remark-shiki-twoslash)
+ * @src https://github.com/shikijs/twoslash/blob/main/packages/remark-shiki-twoslash/README.md
+ * @src https://github.com/shikijs/twoslash/blob/main/packages/shiki-twoslash/README.md
+ * @src Initially inspired by https://github.com/pngwn/MDsveX/issues/212#issuecomment-937574889
  */
 export function shikiTwoslashHighlighter(settings: UserConfigSettings = {}): { highlighter: MDSvexHighlighter } {
   return {
@@ -21,12 +22,13 @@ export function shikiTwoslashHighlighter(settings: UserConfigSettings = {}): { h
       if (hasMetaInstructingToIgnore(meta, settings.ignoreCodeblocksWithCodefenceMeta)) return code;
 
       const twoslash = meta.twoslash ? runTwoSlash(code, lang) : undefined;
-      console.log({twoslash})
       const themes = (settings.themes || DEFAULT_THEMES) as string[];
-      const highlighter = await loadHighlighter({ ...settings, themes });
+      const highlighters = await loadHighlighters({ ...settings, themes });
 
-      const html = themes.map(
-        (themeName) => {
+      const html = highlighters.map(
+        (highlighter) => {
+          const themeName = highlighter.getTheme().name;
+          // .split("/").pop().replace(".json", "")
           return renderCodeToHTML(twoslash?.code || code, lang, meta, { ...settings, themeName }, highlighter, twoslash)
         }
       ).join('');
@@ -44,17 +46,29 @@ function parseCodefence(codefence: string, code: string) {
     const meta = parse(tokens) as Record<string, any>;
     return { lang, meta };
   } catch (error) {
-    throw new Error(`Could not parse the codefence for this code sample \n${code}`);
+    throw new Error(`Could not parse the codefence for this code sample: \n${code}`);
   }
 }
 
-async function loadHighlighter(options: HighlighterOptions): Promise<Highlighter> {
-  try {
-    return await createShikiHighlighter(options);
-  } catch (error) {
-    console.warn(`Could not load ${options.themes} from shiki, using default themes: ${DEFAULT_THEMES}`);
-    return await createShikiHighlighter({ ...options, themes: DEFAULT_THEMES });
+const highlighterCache = new Map<UserConfigSettings, Promise<Highlighter[]>>()
+
+async function loadHighlighters(settings: HighlighterOptions): Promise<Highlighter[]> {
+  if (!highlighterCache.has(settings)) {
+    highlighterCache.set(settings, setupHighlightersDefinedInSettings(settings))
   }
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const highlighters = await highlighterCache.get(settings)!
+  return highlighters;
+}
+
+function setupHighlightersDefinedInSettings(settings: HighlighterOptions): Promise<Highlighter[]> {
+  const themes = settings.themes || (settings.theme ? [settings.theme] : DEFAULT_THEMES)
+  return Promise.all(
+    themes.map(async theme => {
+      const highlighter = await getHighlighter({ ...settings, theme, themes: undefined })
+      return highlighter
+    })
+  )
 }
 
 function hasMetaInstructingToIgnore(meta: Record<string, any>, ignoreCodeblocksWithCodefenceMeta: string[] = []) {
