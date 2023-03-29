fc0eb065c0314e0d3d367a2f51ff4dc3a31a8dd7
jacob-8 <jwrunner7@gmail.com>
2023-03-29 10:00:58 +0800

    don't commit build output

--- packages/rehype-display-link-titles/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/** @type {import('unified').Plugin<[Options?]|void[], Root>} */
-declare function rehypeDisplayLinkTitles(options?: {}): (tree: any, file: any) => void;
-
-export { rehypeDisplayLinkTitles };
