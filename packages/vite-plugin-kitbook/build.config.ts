6e52c368f1bec06e64a41f63cac704b073aab0b4
jacob-8 <jwrunner7@gmail.com>
2023-04-11 08:43:27 +0800

    simplify stringifying of virtual modules import

--- packages/vite-plugin-kitbook/build.config.ts
+++ packages/vite-plugin-kitbook/build.config.ts
@@ -3,14 +3,6 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     'src/index',
-    {
-      builder: 'mkdist',
-      input: './src/virtual/toStringify',
-      outDir: './src/virtual/stringified',
-      format: 'esm',
-      ext: 'js',
-      declaration: false,
-    },
   ],
   declaration: true,
   externals: [
