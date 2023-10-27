b3c2684535dad14c1f397b850fad3f4acf3083bf
jacob-8 <jwrunner7@gmail.com>
2023-10-27 22:11:49 +0800

    add results to comment

--- .github/workflows/comment-formatter.cjs
+++ .github/workflows/comment-formatter.cjs
@@ -1,108 +1,34 @@
 // @ts-check
 
 /**
- * @typedef {object} LighthouseOutputs
- * @prop {Record<string, string>} links
- * @prop {Manifest[]} manifest
- * @property
+ * @param {object} params
+ * @param {string} params.uploadResults
+ * @param {string} params.prNumber
  */
+function makeComment({ uploadResults, prNumber }) {
+  const playwrightReportUrl = `https://storage.googleapis.com/component-snapshots/kitbook-template/branch/${prNumber}/playwright-report/index.html`
 
-/**
- * @typedef {object} Manifest
- * @prop {string} url
- * @prop {boolean} isRepresentativeRun
- * @prop {string} htmlPath
- * @prop {string} jsonPath
- * @prop {Summary} summary
- * @property
- */
+  let comment = '## Playwright Component Snapshots Report'
 
-/**
- * @typedef {object} Summary
- * @prop {number} performance
- * @prop {number} accessibility
- * @prop {number} best-practices
- * @prop {number} seo
- * @prop {number} pwa
- * @property
- */
-
-const formatScore = (/** @type { number } */ score) => Math.round(score * 100)
-function emojiScore(score) {
-  return score >= 0.9 ? '🟢' : score >= 0.5 ? '🟠' : '🔴'
-}
-
-function scoreRow(label, /** @type { number } */ score) {
-  return `| ${emojiScore(score)} ${label} | ${formatScore(score)} |`
-}
-
-function scoreSimple(label, /** @type { number } */ score) {
-  return `${label} ${emojiScore(score)} ${formatScore(score)}`
-}
-
-/**
- * @param {LighthouseOutputs} lighthouseOutputs
- * @param {'slack' | 'pr'} targetPlatform
- */
-function makeComment(lighthouseOutputs, targetPlatform) {
-  let comment = '## ⚡️Lighthouse report'
-
-  if (targetPlatform === 'pr') {
-    comment += `
-<details><summary>Average score across ${lighthouseOutputs.manifest.length} targets: <b>${computeAverageScore(lighthouseOutputs)}%</b></summary>
+  comment += `
+<details><summary>[Playwright Report](${playwrightReportUrl})</summary>
 `
-  }
 
-  for (const manifest of lighthouseOutputs.manifest) {
-    const { url: testedUrl, summary } = manifest
-    const reportUrl = lighthouseOutputs.links[testedUrl]
-
-    if (targetPlatform === 'slack') {
-      // eslint-disable-next-line no-undef
-      comment += ` | Results for ${new URL(testedUrl).pathname}: ${scoreSimple('Performance', summary.performance)}, ${scoreSimple('Accessibility', summary.accessibility)}, ${scoreSimple('Best practices', summary['best-practices'])}, ${scoreSimple('SEO', summary.seo)}, ${scoreSimple('PWA', summary.pwa)}, ${reportUrl}`
-    }
-
-    if (targetPlatform === 'pr') {
-      comment += `
-Results for [${testedUrl}](${testedUrl}) (see [detailed report](${reportUrl}))
-
-| Category | Score |
-| -------- | ----- |
-${scoreRow('Performance', summary.performance)}
-${scoreRow('Accessibility', summary.accessibility)}
-${scoreRow('Best practices', summary['best-practices'])}
-${scoreRow('SEO', summary.seo)}
-${scoreRow('PWA', summary.pwa)}
+  // (![diff-img](https://storage.googleapis.com/component-snapshots/kitbook-template/branch/${prNumber}/image-paths))
+  comment += `
+${uploadResults}
 `
-    }
-  }
 
-  if (targetPlatform === 'pr') {
-    comment += `
+  comment += `
 </details>`
-  }
 
   return comment
 }
 
-/**
- * @param {LighthouseOutputs} lighthouseOutputs
- * @return {string}
- */
-function computeAverageScore(lighthouseOutputs) {
-  let totalScore = 0
-  for (const manifest of lighthouseOutputs.manifest) {
-    const { summary } = manifest
-    totalScore += summary.performance / 4
-    totalScore += summary['best-practices'] / 4
-    totalScore += summary.seo / 4
-    totalScore += summary.pwa / 4
-  }
-  return ((totalScore / lighthouseOutputs.manifest.length) * 100).toFixed(1)
+module.exports = ({ uploadResults, prNumber }) => {
+  return makeComment({ uploadResults, prNumber })
 }
 
-module.exports = ({ lighthouseOutputs, targetPlatform }) => {
-  return makeComment(lighthouseOutputs, targetPlatform)
-}
-
-// from https://blog.logrocket.com/lighthouse-meets-github-actions-use-lighthouse-ci/
+// ignore __-expected.png
+// show __-actual.png
+// link to __-diff.png
