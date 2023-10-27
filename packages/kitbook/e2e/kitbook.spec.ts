6b0f95a975195e9bbcbf864c5baa65ab22d7d15a
jacob-8 <jwrunner7@gmail.com>
2023-10-27 14:09:23 +0800

    use base 64 keys

--- packages/kitbook/e2e/kitbook.spec.ts
+++ packages/kitbook/e2e/kitbook.spec.ts
@@ -5,6 +5,7 @@ import kitbookConfig from '../kitbook.config'
 const skipFiles = [
   '/docs/1-variants/DefaultSlot',
   '/lib/routes/sandbox/[...file]/+page',
+  '/lib/routes/sandbox/[...file]/mockComponents/NeedsPropsToNotError', // this page intentionally errors
 ]
 
 const variantModules = await getVariants({ skipFiles })
