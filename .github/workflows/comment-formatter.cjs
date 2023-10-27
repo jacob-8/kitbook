fcf6eca5f669ddcad4549cff96fc72fa41af592f
jacob-8 <jwrunner7@gmail.com>
2023-10-28 00:27:39 +0800

    fix ordering issue and improve comment

--- .github/workflows/comment-formatter.cjs
+++ .github/workflows/comment-formatter.cjs
@@ -11,14 +11,23 @@ function makeComment({ uploadResults, prNumber }) {
   let comment = '## Playwright Component Snapshots Report'
 
   comment += `
-<details><summary>[Playwright Report](${playwrightReportUrl})</summary>
+<details><summary><a href="${playwrightReportUrl}" target="_blank">Playwright Report</a></summary>
 `
 
-  // (![diff-img](https://storage.googleapis.com/component-snapshots/kitbook-template/branch/${prNumber}/image-paths))
-  comment += `
-${uploadResults}
-`
+  const urls = uploadResults?.split(',') || []
+  for (const url of urls) {
+  /** @type {'actual' | 'diff' | 'expected'} */
+    let type = 'actual'
+    if (url.endsWith('-expected.png'))
+      type = 'expected'
+    else if (url.endsWith('-diff.png'))
+      type = 'diff'
 
+    comment += `
+${type}:
+(![diff-img](https://storage.googleapis.com/component-snapshots/${url}))
+`
+  }
   comment += `
 </details>`
 
@@ -29,6 +38,4 @@ module.exports = ({ uploadResults, prNumber }) => {
   return makeComment({ uploadResults, prNumber })
 }
 
-// ignore __-expected.png
-// show __-actual.png
-// link to __-diff.png
+// const results = 'kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium-retry1/routes/(app)/+page/Second-Mobile-actual.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium-retry1/routes/(app)/+page/Second-Mobile-diff.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium-retry1/routes/(app)/+page/Second-Mobile-expected.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium-retry2/routes/(app)/+page/Second-Mobile-actual.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium-retry2/routes/(app)/+page/Second-Mobile-diff.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium-retry2/routes/(app)/+page/Second-Mobile-expected.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium/routes/(app)/+page/Second-Mobile-actual.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium/routes/(app)/+page/Second-Mobile-diff.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Mobile-chromium/routes/(app)/+page/Second-Mobile-expected.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium-retry1/routes/(app)/+page/Second-Tablet-actual.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium-retry1/routes/(app)/+page/Second-Tablet-diff.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium-retry1/routes/(app)/+page/Second-Tablet-expected.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium-retry2/routes/(app)/+page/Second-Tablet-actual.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium-retry2/routes/(app)/+page/Second-Tablet-diff.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium-retry2/routes/(app)/+page/Second-Tablet-expected.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium/routes/(app)/+page/Second-Tablet-actual.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium/routes/(app)/+page/Second-Tablet-diff.png,kitbook-template/pr/30/test-results/kitbook-routes-app-page-Second-Tablet-chromium/routes/(app)/+page/Second-Tablet-expected.png'
