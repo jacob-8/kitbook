758587aa895715a491e1282c13e49f14438e9556
jacob-8 <jwrunner7@gmail.com>
2023-11-01 10:23:21 +0800

    fix test

--- packages/kitbook/src/lib/plugins/vite/initKitbook.test.ts
+++ packages/kitbook/src/lib/plugins/vite/initKitbook.test.ts
@@ -4,8 +4,8 @@ test(excludeDocFiles, () => {
   expect(excludeDocFiles('+page.svelte')).toBeTruthy()
   expect(excludeDocFiles('+page.js')).toBeTruthy()
 
-  expect(excludeDocFiles('_page.svelte')).toBeFalsy()
-  expect(excludeDocFiles('_layout.svelte')).toBeFalsy()
+  expect(excludeDocFiles('_page.md')).toBeFalsy()
+  expect(excludeDocFiles('_layout.md')).toBeFalsy()
   expect(excludeDocFiles('_page.variants.js')).toBeFalsy()
   expect(excludeDocFiles('+page.d.ts')).toBeFalsy()
   expect(excludeDocFiles('sandbox/mockComponents/+page.svelte')).toBeFalsy()
