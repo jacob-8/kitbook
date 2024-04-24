e2ac9799ddb04329a957b357693ebce12e32cddc
jacob-8 <jwrunner7@gmail.com>
2024-04-24 08:06:16 +0800

    update vitePreprocess import for SvelteKit 2 upgrade

--- packages/kitbook/svelte.config.js
+++ packages/kitbook/svelte.config.js
@@ -1,6 +1,6 @@
 // @ts-check
 import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import UnoCSS from '@unocss/svelte-scoped/preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
