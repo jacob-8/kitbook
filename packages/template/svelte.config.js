d0cfe97a6dfd38a745c8ded81309ae2a75d4f3a5
Jacob Bowdoin <7559478+jacob-8@users.noreply.github.com>
2024-04-24 08:13:33 +0800

    update packages (#37)

--- packages/template/svelte.config.js
+++ packages/template/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto'
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,6 +11,7 @@ const config = {
   kit: {
     adapter: adapter(),
   },
+
   onwarn: (warning, handler) => {
     if (warning.code.startsWith('a11y-'))
       return
